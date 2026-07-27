409c58c8b256137fd4f213608a567add71ad4741
rama <ramathapa130@gmail.com>
2026-07-27 21:33:57 +0545

    kj

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+// const MONGODB_URI="mongodb+srv://shortcourseuser:cg3rTKAq6mnMKTZ0@orchid.bpzyobi.mongodb.net/shortcourse";
 
+// if (!MONGODB_URI) {
+//   throw new Error("Please define MONGODB_URI in .env.local");
+// }
+const MONGODB_URI="mongodb://shortcourseuser:cg3rTKAq6mnMKTZ0@ac-bx9hhjl-shard-00-00.bpzyobi.mongodb.net:27017,ac-bx9hhjl-shard-00-01.bpzyobi.mongodb.net:27017,ac-bx9hhjl-shard-00-02.bpzyobi.mongodb.net:27017/?ssl=true&replicaSet=atlas-ubomtq-shard-0&authSource=admin";
 if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable in .env.local"
-  );
+  throw new Error("Please define the MONGODB_URI environment variable in .env.local")
 }
 
 interface MongooseCache {
@@ -18,22 +20,20 @@ declare global {
   var mongoose: MongooseCache | undefined;
 }
 
-const cached: MongooseCache = global.mongoose ?? {
+const cached: MongooseCache = global.mongoose || {
   conn: null,
   promise: null,
 };
 
-if (!global.mongoose) {
-  global.mongoose = cached;
-}
+global.mongoose = cached;
 
-export async function connectDB() {
+export async function connectDB(): Promise<typeof mongoose> {
   if (cached.conn) {
     return cached.conn;
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI);
+    cached.promise = mongoose.connect(MONGODB_URI!);
   }
 
   cached.conn = await cached.promise;
