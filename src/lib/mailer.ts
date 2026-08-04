ccea0f00d8e9fc7c4e90f7840963205c2f501405
rama <ramathapa130@gmail.com>
2026-08-04 21:21:11 +0545

    d

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,13 +1,12 @@
 import nodemailer from "nodemailer";
 
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "Loaded" : "Missing");
 
 export const transporter = nodemailer.createTransport({
-
-  service:"gmail",
-
-  auth:{
-    user:process.env.EMAIL_USER,
-    pass:process.env.EMAIL_PASS,
-  }
-
+  service: "gmail", // or Zoho configuration
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
